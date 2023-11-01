6a09329ec22bc8482184cf0bb5554f684c09e4e7
jayarnim <jayarnim727@gmail.com>
2023-11-01 17:22:02 +0900

    upload const package common

---
+++ common/data.py
@@ -0,0 +1,65 @@
+# After Cleansing
+# 0-0_MERGE
+PATH_DF_MERGE = "https://media.githubusercontent.com/media/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/df_merge.csv"
+
+# 0-1_DROP
+PATH_DF_DROP = ""
+
+
+# Raw Data Set
+# 상주인구
+PATH_RESIDENT = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%83%81%EC%A3%BC%EC%9D%B8%EA%B5%AC.csv"
+
+# 생활인구
+PATH_LIVING = "https://media.githubusercontent.com/media/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%83%9D%ED%99%9C%EC%9D%B8%EA%B5%AC.csv"
+
+# 직장인구
+PATH_WORKING = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%A7%81%EC%9E%A5%EC%9D%B8%EA%B5%AC.csv"
+
+# 소득소비
+PATH_INCOME_CONSUME = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%86%8C%EB%93%9D%EC%86%8C%EB%B9%84.csv"
+
+# 아파트
+PATH_APARTMENT = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%95%84%ED%8C%8C%ED%8A%B8.csv"
+
+# 추정매출
+PATH_SALES_2017 = "https://media.githubusercontent.com/media/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%B6%94%EC%A0%95%EB%A7%A4%EC%B6%9C_2017.csv"
+PATH_SALES_2018 = "https://media.githubusercontent.com/media/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%B6%94%EC%A0%95%EB%A7%A4%EC%B6%9C_2018.csv"
+PATH_SALES_2019 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%B6%94%EC%A0%95%EB%A7%A4%EC%B6%9C_2019.csv"
+PATH_SALES_2020 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%B6%94%EC%A0%95%EB%A7%A4%EC%B6%9C_2020.csv"
+PATH_SALES_2021 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%B6%94%EC%A0%95%EB%A7%A4%EC%B6%9C_2021.csv"
+PATH_SALES_2022 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%B6%94%EC%A0%95%EB%A7%A4%EC%B6%9C_2022.csv"
+PATH_SALES_2023 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%B6%94%EC%A0%95%EB%A7%A4%EC%B6%9C_2023.csv"
+
+# 점포현황
+PATH_STORE_2017 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%A0%90%ED%8F%AC_2017.csv"
+PATH_STORE_2018 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%A0%90%ED%8F%AC_2018.csv"
+PATH_STORE_2019 = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%A0%90%ED%8F%AC_2019.csv"
+PATH_STORE_2020 = "https://media.githubusercontent.com/media/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%A0%90%ED%8F%AC_2023-2020.csv"
+
+# 상권변화지표
+PATH_CHANGE_INDICATOR = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%83%81%EA%B6%8C%EB%B3%80%ED%99%94%EC%A7%80%ED%91%9C.csv"
+
+# 집객시설
+PATH_FACILITY = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%A7%91%EA%B0%9D%EC%8B%9C%EC%84%A4.csv"
+
+# 상권 영역
+PATH_AREA = "https://media.githubusercontent.com/media/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%98%81%EC%97%AD.csv"
+
+# 상권 정의
+PATH_DIFINE = "https://raw.githubusercontent.com/jayarnim/project-Comparative_Analysis_of_Changes_in_Influence_of_Market_Demand_Determinants/main/data/%EC%83%81%EA%B6%8C-%EC%A0%95%EC%9D%98.csv"
+
+# RAW DATA DICT
+RAWDICT = {
+    "상주인구":[PATH_RESIDENT],
+    "생활인구":[PATH_LIVING],
+    "직장인구":[PATH_WORKING],
+    "소득소비":[PATH_INCOME_CONSUME],
+    "아파트":[PATH_APARTMENT],
+    "추정매출":[PATH_SALES_2017, PATH_SALES_2018, PATH_SALES_2019, PATH_SALES_2020, PATH_SALES_2021, PATH_SALES_2022, PATH_SALES_2023],
+    "점포현황":[PATH_STORE_2017, PATH_STORE_2018, PATH_STORE_2019, PATH_STORE_2020],
+    "상권변화지표":[PATH_CHANGE_INDICATOR],
+    "집객시설":[PATH_FACILITY],
+    "상권영역":[PATH_AREA],
+    "상권정의":[PATH_DIFINE]
+    }
